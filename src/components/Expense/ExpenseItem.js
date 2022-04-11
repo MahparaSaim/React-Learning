effe914e97516fdd44b35bbcd8ba0c25e244525d
Mahpara Saim <mah.para.saim@gmail.com>
2022-04-11 23:41:22 +0500

    useStae hook

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -2,15 +2,21 @@ import "./ExpenseItem.css";
 
 import Card from "../UI//Card";
 import ExpenseDate from "../Expense/ExpenseDate";
-import React from "react";
+import React,{useState} from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle('Updated')
+    console.log("Clicke Handler");
+  };
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
         <ExpenseDate date={props.date} />
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Chhange Title</button>
       </div>
     </Card>
   );
