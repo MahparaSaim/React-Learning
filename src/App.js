1b16795e35921b5b24f6137164a453ae3e909980
Mahpara Saim <mah.para.saim@gmail.com>
2022-04-17 05:43:13 +0500

    lifiting ythe state up

--- src/App.js
+++ src/App.js
@@ -4,14 +4,41 @@ import Expense from "./components/Expense/Expense";
 import NewExpense from './components/NewExpense/NewExpense'
 
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Car Insurance 1",
+      amount: 295.96,
+      date: Date(2022, 4, 5),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance 2",
+      amount: 295.96,
+      date: Date(2021, 4, 5),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance 3",
+      amount: 295.96,
+      date: Date(2020, 2, 5),
+    },
+    {
+      id: "e4",
+      title: "Car Insurance 4",
+      amount: 295.96,
+      date: Date(2019, 3, 5),
+    },
+  ];
+ 
   const addExpenseHandler = (expenses) => {
     console.log("In the App.js ");
     console.log(expenses);
   };
   return (
     <>
-      <NewExpense onAddExpense={ addExpenseHandler}/>
-      <Expense />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expense items={expense} />
     </>
   );
 }
--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -1,60 +1,43 @@
 import "./Expense.css";
 
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 import ExpenseItem from "../Expense/ExpenseItem";
-import React from "react";
+import React, {useState} from "react";
+import ExpensesFilter from "./ExpensesFilter";
 
-function Expense() {
-  const expense = [
-    {
-      id: "e1",
-      title: "Car Insurance 1",
-      amount: 295.96,
-      date: Date(2022, 4, 5),
-    },
-    {
-      id: "e2",
-      title: "Car Insurance 2",
-      amount: 295.96,
-      date: Date(2021, 4, 5),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance 3",
-      amount: 295.96,
-      date: Date(2020, 2, 5),
-    },
-    {
-      id: "e4",
-      title: "Car Insurance 4",
-      amount: 295.96,
-      date: Date(2019, 3, 5),
-    },
-  ];
+function Expense(props) {
+      const [filterYear, setFilterYear] = useState('2020');
+
+  const filerChangeHander = (selectYear) => {
+    console.log('Expenses jS')
+    setFilterYear(selectYear);
+  }
   return (
-    <Card className="expense">
-      <h2> Hello </h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expense">
+        <ExpensesFilter selected={filterYear} onFilterChange={filerChangeHander} />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
